435da97ffb30286d5b0284d2e3a1ca930e6d178c
OzzyD07 <ozancan976@gmail.com>
2024-08-21 16:31:14 +0300

    updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import keyboard
 import components.addPerson as add
-import components.getDirectory as get1
+import components.getDirectory as get
 import components.search as search
 
 class Menu:
@@ -7,11 +8,16 @@ class Menu:
     def __init__(self, options):
         self.options = options
         self.max_width = len(max(self.options, key=len))
-
+        self.cursesPlace = 0 
+            
     def print_options(self):
-        for option in self.options:
+        for index, option in enumerate(self.options):
             padding = " " * (self.max_width - len(option))
-            print(f"║ {option}{padding} ║")
+            if index == self.cursesPlace:
+                print(f"║>{option}{padding} ║")
+            else: 
+                print(f"║ {option}{padding} ║")
+
 
     def draw_frame(self):
         print(f"{' ' * (int(self.max_width / 2 - 2))}PROJELER")
@@ -22,36 +28,32 @@ class Menu:
         print(f"║ {' ' * self.max_width} ║")
         print(f"╚{top_bottom}╝")
 
-    def get_selection(self):
-        while True:
-            try:
-                selection = int(input("Bir seçenek seçin: "))
-                if selection == 0:
-                    print("Programdan çıkılıyor...")
-                    return None
-                elif selection > len(self.options) - 1:
-                    print("Geçersiz seçenek!")
-                else:
-                    print(self.options[selection - 1], "seçildi.")
-                    return selection
-            except ValueError:
-                print("Lütfen bir sayı girin.")
 
     def run(self):
         while True:
             self.draw_frame()
-            selection = self.get_selection()
-            if selection is None:
-                break
-            elif selection == 1:
-                get1.printDirectory()
-            elif selection == 2:
-                add.addPerson()
-            elif selection == 3:
-                search.searchWithName()
-            elif selection == 4:
-                search.searchWithNo()
-    
+            key = keyboard.read_event()
+            if key.event_type == "down" and key.name == "up":
+                self.cursesPlace = (self.cursesPlace - 1) % len(self.options)
+            elif key.event_type == "down" and key.name == "down":
+                self.cursesPlace = (self.cursesPlace + 1) % len(self.options)
+            elif key.event_type == "down" and key.name == "enter":
+                self.select_option()
+
+ 
+    def select_option(self):
+        option = self.options[self.cursesPlace]
+        if option.startswith("1"):
+            get.printDirectory()
+        elif option.startswith("2"):
+            add.addPerson()
+        elif option.startswith("3"):
+            search.searchWithName()
+        elif option.startswith("4"):
+            search.searchWithNo()
+        elif option.startswith("0"):
+            print("Çıkış yapılıyor...")
+            exit()
 
 if __name__ == '__main__':
     options = [
